f837d47ee391a74031a2ee896eed3d3f1d577029
vitaliy-pg <aljndrvt@myuax.com>
2023-12-14 12:18:27 +0100

    estructura del estudiante

--- Estudiantes.cpp
+++ Estudiantes.cpp
@@ -2,3 +2,13 @@
 #include <vector>
 #include <string>
 #include <algorithm>  // Necesario para std::find en C++
+
+struct EstudianteCpp {
+    std::string nombre;
+    int edad;
+    float promedio;
+    // Lista de materias en las que está inscrito el estudiante
+    std::vector<std::string> materias;
+    // Lista de asistencias
+    std::vector<struct AsistenciaCpp> asistencias;
+};
