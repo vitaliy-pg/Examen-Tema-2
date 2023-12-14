4e5d11acfe09bea950756f03684e21caca0f52d0
vitaliy-pg <aljndrvt@myuax.com>
2023-12-14 11:56:22 +0100

    implemento la clase y estructura estudiante

--- Estudiantes.cpp
+++ Estudiantes.cpp
@@ -2,3 +2,9 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+struct Estudiante {
+    std::string nombre;
+    int edad;
+    float promedio;
+    std::vector<std::string> materias;
+    std::vector<std::string> asistencias;
